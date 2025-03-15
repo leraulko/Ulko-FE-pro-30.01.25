4efc9c799b490a06afa5ed2da61bad65d6f7eacc
Lera Ulko <leraulko@MacBook-Pro-Lera.local>
2025-03-15 21:09:11 +0200

    added hw 12.1

---
+++ HW12.1/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// 12.1 На сторінці є дві кнопки. При натисканні на першу кнопку користувач повинен ввести в prompt посилання, при натисканні на другу – переадресовується на інший сайт (за раніше введеним посиланням).
+let userLink = '';
+
+document.querySelector('.button-prompt').addEventListener('click', () => {
+    do {
+        userLink = prompt('Enter proper link u wanna go to, which starts with "https://" :');
+    } while (!userLink || !isNaN(userLink) || !userLink.startsWith('https://'));
+
+});
+
+document.querySelector('.button-redirect').addEventListener('click', () => {
+    location.href = userLink;
+});
