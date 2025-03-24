30840c22323cabd3acc55a286425fa5f54af6639
Lera Ulko <leraulko@MacBook-Pro-Lera.local>
2025-03-24 17:17:58 +0200

    added practice #13

---
+++ Practice 13/scripts/orders.js
@@ -0,0 +1,76 @@
+const categories = [{
+    id: 1,
+    name: 'Laptops',
+    products: [{
+        id: 1,
+        name: 'Macbook',
+        price: 19000,
+        description: 'Premium laptop from Apple, known for macOS, sleek design, high-quality Retina displays, and great performance.'
+      },
+      {
+        id: 2,
+        name: 'Dell',
+        price: 10100,
+        description: 'Reliable and versatile laptops, popular for business and gaming, offering a balance of performance and durability.'
+      },
+      {
+        id: 3,
+        name: 'Asus',
+        price: 10300,
+        description: 'A wide range of laptops from budget-friendly to high-end gaming and creative workstations, known for innovation.'
+      },
+      {
+        id: 4,
+        name: 'Acer',
+        price: 10500,
+        description: 'Affordable and efficient laptops for everyday use, with some strong gaming and productivity options.'
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Phones',
+    products: [{
+        id: 5,
+        name: 'Samsung',
+        price: 19000,
+        description: 'Leading Android smartphone brand, offering cutting-edge technology, high-quality displays, and strong cameras.'
+      },
+      {
+        id: 6,
+        name: 'iPhone',
+        price: 10100,
+        description: 'Apple’s flagship smartphone, known for smooth iOS performance, premium design, and ecosystem integration.'
+      },
+      {
+        id: 7,
+        name: 'Huawei',
+        price: 10300,
+        description: 'Feature-rich smartphones with powerful cameras and strong hardware, though limited in Google services.'
+      },
+      {
+        id: 8,
+        name: 'Honor',
+        price: 10500,
+        description: 'A sub-brand of Huawei, offering stylish and affordable smartphones with decent performance.'
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Processors',
+    products: [{
+        id: 9,
+        name: 'Intel',
+        price: 19000,
+        description: 'Well-known for reliable and high-performance processors, widely used in gaming, business, and everyday computing.'
+      },
+      {
+        id: 10,
+        name: 'AMD',
+        price: 10100,
+        description: 'Strong competitor to Intel, offering great multi-core performance, excellent for gaming and productivity at a competitive price.'
+      },
+    ],
+  },
+];
