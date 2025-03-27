5aa5e48c2071e24ceba2b94fdab3e54d4c963d02
Lera Ulko <leraulko@MacBook-Pro-Lera.local>
2025-03-27 15:27:08 +0200

    added changes for dynamic img addition and appearance

--- HW14/index.js
+++ HW14/index.js
@@ -1,21 +1,29 @@
 'use strict';
 
+const imageArr = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'];
+const imageFolder = 'pics/';
+
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
-const sliderImages = document.querySelectorAll('.slider img');
+const sliderImages = document.querySelector('.slider');
 const sliderDots = document.querySelector('.nav-dots');
 let currentIndex = 0;
 
 
-sliderImages.forEach((el, index) => {
+imageArr.forEach((image, index) => {
+    let img = document.createElement('img');
+    img.src = `${imageFolder}${image}`;
+    if (index === currentIndex) {
+        img.classList.add('active');
+    }
+    sliderImages.appendChild(img);
+
     const dot = document.createElement('span');
     dot.classList.add('dot');
-
+    dot.setAttribute('data-index', index);
     if (index === currentIndex) {
         dot.classList.add('active');
     }
-    
-    dot.setAttribute('data-index', index);
     sliderDots.appendChild(dot);
 });
 
@@ -23,7 +31,8 @@ const dots = document.querySelectorAll('.dot');
 
 
 function changeSlider() {
-    sliderImages.forEach((img, index) => {
+    const imgs = document.querySelectorAll('.slider img');
+    imgs.forEach((img, index) => {
         img.classList.toggle('active', index === currentIndex);
     });
 
@@ -32,7 +41,7 @@ function changeSlider() {
     });
 
     prevBtn.classList.toggle('hidden', currentIndex === 0);
-    nextBtn.classList.toggle('hidden', currentIndex === sliderImages.length - 1);
+    nextBtn.classList.toggle('hidden', currentIndex === imageArr.length - 1);
 }
 
 prevBtn.addEventListener('click', () => {
@@ -43,7 +52,7 @@ prevBtn.addEventListener('click', () => {
 });
 
 nextBtn.addEventListener('click', () => {
-    if (currentIndex < sliderImages.length - 1) {
+    if (currentIndex < imageArr.length - 1) {
         currentIndex++;
         changeSlider();
     }
